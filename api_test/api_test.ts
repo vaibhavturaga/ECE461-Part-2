1805a9e2b172b4ca24f5c2738ac89ddca2434254
benboardley <bboard1202@gmail.com>
2023-09-04 14:23:59 -0400

    get github from npm

--- api_test/api_test.ts
+++ api_test/api_test.ts
@@ -8,7 +8,7 @@ if (!token) {
     process.exit(1); // Exit the script with an error code
   }
 
-async function getPublicRepo(): Promise<void> {
+async function getPublicRepo(repo: string, org: string): Promise<void> {
     try{
         const axiosInstance = axios.create({
             baseURL: 'https://api.github.com/',
@@ -17,7 +17,8 @@ async function getPublicRepo(): Promise<void> {
                 Accept: 'application/json'
             },
         });
-        const endpoint: string = 'repos/apache/airflow'
+        const endpoint: string = `repos/${org}/${repo}`
+        console.log(endpoint)
 
         const response: AxiosResponse<any[]> = await axiosInstance.get(endpoint);
         console.log('Response:', response);
@@ -27,4 +28,39 @@ async function getPublicRepo(): Promise<void> {
     }
 }
 
-getPublicRepo();
+//getPublicRepo();
+
+async function getGithubRepoForNPMurl(url: string): Promise<string | null>{
+    const response = await axios.get(url)
+
+    if (response.status !== 200) {
+        console.error(`Error: Unable to fetch package information for ${url}`);
+        return null;
+    }
+
+    const packageInfo = response.data
+    console.log(packageInfo)
+    if (packageInfo.repository && packageInfo.repository.url) {
+        return packageInfo.repository.url;
+    }
+
+    return null;
+}
+const npm = 'https://registry.npmjs.com/lodash'
+getGithubRepoForNPMurl(npm).then((githubRepoUrl) => {
+  if (githubRepoUrl) {
+    const urlParts = githubRepoUrl.split('/');
+    console.log(`The GitHub repository for ${npm} is: ${githubRepoUrl}`);
+    const org = urlParts[urlParts.length - 2];
+    const repo = urlParts[urlParts.length - 1].split('.')[0];
+    console.log(org)
+    console.log(repo)
+    getPublicRepo(repo,org);
+  }
+  else{
+    console.log('repo not found')
+  }
+})
+.catch((error) => {
+  console.error(`An error occurred: ${error.message}`);
+});
