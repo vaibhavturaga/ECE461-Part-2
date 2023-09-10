6cd7bb094e2d2ca3decd0b0d95cca3b783dc4cfe
benboardley <bboard1202@gmail.com>
2023-09-10 19:05:20 -0400

    setup function

--- tools/api.ts
+++ tools/api.ts
@@ -1,14 +1,7 @@
 import axios, {AxiosResponse} from 'axios';
 import * as dotenv from 'dotenv'
 import { getPackageManifest } from 'query-registry';
-
-dotenv.config({ path: '.env' });
-const token: string | undefined = process.env.GITHUB_API_KEY;
-
-if (!token) {
-    console.error('GitHub API token not found in the .env file');
-    process.exit(1); // Exit the script with an error code
-  }
+const Promise = require('bluebird');
 
 /****************************************************************************************************************************************
  * repoConnection
@@ -38,7 +31,8 @@ if (!token) {
         })();
   */
 class repoConnection{
-  url: string | null; 
+  url: string | null;
+  npm_url: string | null = null; 
   githubkey: string | null;
   repo: string;
   org: string;
@@ -75,6 +69,7 @@ class repoConnection{
   }
   async processUrl(url: string): Promise<string | null> {
     if (url.includes("npmjs")) {
+      this.npm_url = url;
       try {
         const githubRepoUrl = await this.queryNPM(url);
         if (githubRepoUrl) {
@@ -233,19 +228,30 @@ class repoCommunicator {
   }
 }
 
-const urls: string[] = ['https://www.npmjs.com/package/browserify', 'https://github.com/cloudinary/cloudinary_npm', 'https://www.npmjs.com/package/express', 'https://github.com/nullivex/nodist', 'https://github.com/lodash/lodash',];
-(async () => {
+async function setupCommunication(urls: string[]) {
+  dotenv.config({ path: '.env' });
+  const token: string | undefined = process.env.GITHUB_API_KEY;
+
+  if (!token) {
+      console.error('GitHub API token not found in the .env file');
+      process.exit(1); // Exit the script with an error code
+    }
   const startUsingPromiseAll = performance.now();
   
   // Create an array of promises to initialize connections and communicators for each URL
-  const initializationPromises = urls.map(async (url) => {
+  /*
+  const connectionsAndCommunicators = await Promise.all(urls.map(async (url) => {
     const connection = await repoConnection.create(url, token);
     const communicator = await repoCommunicator.create(connection);
     return { connection, communicator };
-  });
-  // Use Promise.all to initialize connections and communicators concurrently for all URLs
-  const connectionsAndCommunicators = await Promise.all(initializationPromises);
+  }));*/
 
+  const connectionsAndCommunicators = await Promise.map(urls, async (url: string) => {
+    const connection = await repoConnection.create(url, token);
+    const communicator = await repoCommunicator.create(connection);
+    return { connection, communicator };
+  }, { concurrency: 10 });
+  //console.log(connectionsAndCommunicators[0])
   const endUsingPromiseAll = performance.now();
   const usingPromiseAllTime = endUsingPromiseAll - startUsingPromiseAll;
   console.log(`Promise.all time: ${usingPromiseAllTime}`);
@@ -263,4 +269,7 @@ const urls: string[] = ['https://www.npmjs.com/package/browserify', 'https://git
   
   //await getinfo.compareRetrieveMethods();
   //console.log(getinfo.issues)
-})();
+}
+
+const urls: string[] = ['https://www.npmjs.com/package/browserify', 'https://github.com/cloudinary/cloudinary_npm', 'https://www.npmjs.com/package/express', 'https://github.com/nullivex/nodist', 'https://github.com/lodash/lodash','https://www.npmjs.com/package/express', 'https://github.com/nullivex/nodist', 'https://github.com/lodash/lodash','https://www.npmjs.com/package/browserify', 'https://github.com/cloudinary/cloudinary_npm', 'https://www.npmjs.com/package/express', 'https://github.com/nullivex/nodist', 'https://github.com/lodash/lodash','https://www.npmjs.com/package/express', 'https://github.com/nullivex/nodist', 'https://github.com/lodash/lodash','https://www.npmjs.com/package/browserify', 'https://github.com/cloudinary/cloudinary_npm', 'https://www.npmjs.com/package/express', 'https://github.com/nullivex/nodist', 'https://github.com/lodash/lodash','https://www.npmjs.com/package/express', 'https://github.com/nullivex/nodist', 'https://github.com/lodash/lodash'];
+setupCommunication(urls);
