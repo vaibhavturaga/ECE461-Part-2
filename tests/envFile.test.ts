b06e5dc55fe18fa2505ea5489dc1933ad11196d1
hgruler24 <71056537+hgruler24@users.noreply.github.com>
2023-09-24 19:29:13 -0400

    Update envFile.test.ts

--- tests/envFile.test.ts
+++ tests/envFile.test.ts
@@ -2,25 +2,6 @@ import { readEnv } from '../src/envFileReader';
 import { describe, test, expect } from '@jest/globals';
 
 describe('env_reader', () => {
-    // Function to check if a url file exists, if not, create it
-    const ensureLogFileExists = (logFilePath: string) => {
-        if (!fs.existsSync(logFilePath)) {
-            fs.writeFileSync(logFilePath, '');
-        }
-    };
-
-    beforeAll(() => {
-        // Ensure the logs directory exists before running the tests
-        const logsDirectory = path.join(__dirname, '../logs');
-        if (!fs.existsSync(logsDirectory)) {
-            fs.mkdirSync(logsDirectory);
-        }
-
-        // Check and create log files if necessary
-        ensureLogFileExists(path.join(__dirname, '../logs/error.log'));
-        ensureLogFileExists(path.join(__dirname, '../logs/combined.log'));
-    });
-    
     test('returns correct object', async () => {
 
         // Grab the env object
