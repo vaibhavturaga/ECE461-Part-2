4d771146d4874c4f8b296e07260617e866eb9cf0
hgruler24 <71056537+hgruler24@users.noreply.github.com>
2023-09-24 19:34:37 -0400

    Update urlFile.test.ts

--- tests/urlFile.test.ts
+++ tests/urlFile.test.ts
@@ -1,9 +1,28 @@
 import { readURLs } from '../src/urlFileReader';
+import logger from '../src/logger';
+import * as fs from 'fs';
 import * as path from 'path';
 import { describe, test, expect } from '@jest/globals';
 
 describe('URL_FILE', () => {
     // Function to check if a url file exists, if not, create it
+    const ensureLogFileExists = (logFilePath: string) => {
+        if (!fs.existsSync(logFilePath)) {
+            fs.writeFileSync(logFilePath, '');
+        }
+    };
+
+    beforeAll(() => {
+        // Ensure the logs directory exists before running the tests
+        const logsDirectory = path.join(__dirname, '../logs');
+        if (!fs.existsSync(logsDirectory)) {
+            fs.mkdirSync(logsDirectory);
+        }
+
+        // Check and create log files if necessary
+        ensureLogFileExists(path.join(__dirname, '../logs/error.log'));
+        ensureLogFileExists(path.join(__dirname, '../logs/combined.log'));
+    });
     
     test('returns correct array', async () => {
         let testArray = ["https://www.npmjs.com/package/is-regex", "https://www.npmjs.com/package/path-to-regexp", "https://www.npmjs.com/package/escape-string-regexp", "https://github.com/es-shims/RegExp.prototype.flags", "https://github.com/ljharb/safe-regex-test", "https://github.com/npm/hosted-git-info", "https://www.npmjs.com/package/@octokit/rest"]
@@ -15,14 +34,21 @@ describe('URL_FILE', () => {
         expect(JSON.stringify(urls)).toBe(JSON.stringify(testArray));
     });
 
-    test('errors when file not found', async () => {
-        const errorSpy = jest.spyOn(global.console, 'error');
+    test('logs error messages correctly', async () => {
+        // Log an error message
+        await readURLs('doesNotExist.txt');
 
-        // Try to use a non existant file
-        let urls = await readURLs('notReal.txt');
+        // Construct the path to the error.log file
+        const errorLogFilePath = path.join(__dirname, '../logs/error.log');
 
-        // Expect that the return array contains the urls
-        expect(errorSpy).toBeCalled();
+        // Wait for a short time to allow Winston to create and write to the log file
+        await new Promise((resolve) => setTimeout(resolve, 100));
+
+        // Read the contents of the error.log file
+        const errorLogFileContents = fs.readFileSync(errorLogFilePath, 'utf8');
+
+        // Expect that the error.log file contains the error message
+        expect(errorLogFileContents).toContain('File not found at:');
     });
 
     // Additional test cases can be added here as needed
