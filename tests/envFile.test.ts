a0fa2161aae767aa818b26c3d6545f893c3c6623
hgruler24 <71056537+hgruler24@users.noreply.github.com>
2023-09-24 14:11:16 -0400

    Created tests for file reading

---
+++ tests/envFile.test.ts
@@ -0,0 +1,20 @@
+import { readEnv } from '../src/envFileReader';
+import { describe, test, expect } from '@jest/globals';
+
+describe('env_reader', () => {
+    // Function to check if a url file exists, if not, create it
+    
+    test('returns correct object', async () => {
+
+        // Grab the env object
+        let env = await readEnv();
+
+        // Expect that the return array contains the urls
+        expect(parseInt(env.logLevel)).toBeGreaterThanOrEqual(1);
+        expect(parseInt(env.logLevel)).toBeLessThanOrEqual(3);
+        expect(env.token).toBeDefined();
+        expect(env.logFile).toBeDefined();
+    });
+
+    // Additional test cases can be added here as needed
+});
