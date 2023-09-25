ed34de13d3c43e2f025083e431853322589f09c8
hgruler24 <71056537+hgruler24@users.noreply.github.com>
2023-09-24 21:21:21 -0400

    Update envFileReader.ts

--- src/envFileReader.ts
+++ src/envFileReader.ts
@@ -1,29 +1,12 @@
-import logger from '../src/logger';
-import * as fsPromise from 'fs/promises'; 
-import * as path from 'path';
-
 export const readEnv = async () => {
-    let token = "";
-    let logLevel = "";
-    let logFile = "";
+    let token: string | undefined = "";
+    let logLevel: string | undefined = "";
+    let logFile: string | undefined = "";
+    const dotenv = require('dotenv');
+    dotenv.config({ path: '.env' });
+    token = process.env.GITHUB_TOKEN;
+    logLevel = process.env.LOG_LEVEL;
+    logFile = process.env.LOG_FILE;
     
-    await fsPromise.open(path.join(__dirname, '../.env'), 'r')
-        .then(async (response) => {
-            for await (const line of response.readLines()){
-                if(line.includes("GITHUB_TOKEN=")){
-                    token = line.replace("GITHUB_TOKEN=", "");
-                }
-                else if(line.includes("LOG_LEVEL=")){
-                    logLevel = line.replace("LOG_LEVEL=", "");
-                }
-                else if(line.includes("LOG_FILE=")){
-                    logFile = line.replace("LOG_FILE=", "");
-                }
-            }
-        })
-        .catch(() => {
-            logger.error(`.env file not found`)
-        });
-
     return {token:token, logLevel:logLevel, logFile:logFile};
 };
