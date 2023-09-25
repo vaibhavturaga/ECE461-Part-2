b39fc00f91d732aaf296c9aa002c0a944dce327c
Nathaniel Bielanski <nbielans@purdue.edu>
2023-09-24 23:16:58 -0400

    jestReporter.js and config files

--- dist/jestReporter.js
+++ dist/jestReporter.js
@@ -1,64 +1,25 @@
-const { CoverageReporter } = require('@jest/reporters');
-// const {coverageMap} = require('istanbul-lib-coverage');
+const { readFile } = require('fs');
+const { join } = require('path');
 
-class CustomReporter extends CoverageReporter{
-  constructor(globalConfig, reporterOptions, reporterContext) {
-    super(globalConfig, reporterContext);
-    this._globalConfig = globalConfig;
-    this._options = reporterOptions;
-    this._context = reporterContext;
+// Gitlab Regex: Total Coverage: (\d+.\d+ %)
+module.exports = class CoverageReporter {
+  constructor(globalConfig) {
+    this.globalConfig = globalConfig;
+    this.jsonSummary = join(this.globalConfig.coverageDirectory, 'coverage-summary.json');
   }
-
-  // async onRunComplete(testContexts, aggregatedResults) {
-  //   await this._addUntestedFiles(testContexts);
-  //   const {map, reportContext} = await this._getCoverageResult();
-  //   try {
-  //     const coverageReporters = this._globalConfig.coverageReporters || [];
-  //     if (!this._globalConfig.useStderr && coverageReporters.length < 1) {
-  //       coverageReporters.push('text-summary');
-  //     }
-  //     coverageReporters.forEach(reporter => {
-  //       let additionalOptions = {};
-  //       if (Array.isArray(reporter)) {
-  //         [reporter, additionalOptions] = reporter;
-  //       }
-  //       _istanbulReports()
-  //         .default.create(reporter, {
-  //           maxCols: process.stdout.columns || Infinity,
-  //           ...additionalOptions
-  //         })
-  //         .execute(reportContext);
-  //     });
-  //     aggregatedResults.coverageMap = map;
-  //   } catch (e) {
-  //     console.error(
-  //       _chalk().default.red(`
-  //       Failed to write coverage reports:
-  //       ERROR: ${e.toString()}
-  //       STACK: ${e.stack}
-  //     `)
-  //     );
-  //   }
-  //   this._checkThreshold(map);
-  // }
-
-  async onRunComplete(testContexts, results) {
+  async onRunComplete(contexts, results) {
     const totalTests = results.numTotalTests;
     const passedTests = results.numPassedTests;
   
+    const coverage = require(this.jsonSummary);
+    const totalSum = ['lines', 'statements', 'functions', 'branches']
+      .map(i => coverage.total[i].pct)
+      .reduce((a, b) => a + b, 0)
+    const avgCoverage = totalSum / 4
+
     console.log(`Total: ${totalTests}`);
     console.log(`Passed: ${passedTests}`);
-    if (results._coverageMap) {
-      const coverage = results._coverageMap.getCoverageSummary();
-      const totalCoverage = (
-        (coverage.lines.pct + coverage.branches.pct + coverage.functions.pct + coverage.statements.pct) / 4
-      ).toFixed(2);
-      console.log(`Coverage: ${totalCoverage}%`);
-      console.log(`${passedTests}/${totalTests} test cases passed. ${totalCoverage}% line coverage achieved.`);
-    } else {
-      console.log(`${passedTests}/${totalTests} test cases passed. NULL`);
-    }
-  }  
+    console.log(`Coverage: ${avgCoverage.toFixed(2)} %`)
+    console.log(`${passedTests}/${totalTests} test cases passed. ${avgCoverage.toFixed(2)}% line coverage achieved.`);
+  } 
 }
-
-module.exports = CustomReporter;
